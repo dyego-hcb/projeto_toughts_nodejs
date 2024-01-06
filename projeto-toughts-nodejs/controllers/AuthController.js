bedb59a259edaa926ffe51103d1d993a4966d742
Dyego Henrique Costa Braga <dyego.hcb@gmail.com>
2024-01-06 17:42:13 -0300

    Finish Autenticate

--- projeto-toughts-nodejs/controllers/AuthController.js
+++ projeto-toughts-nodejs/controllers/AuthController.js
@@ -7,6 +7,34 @@ module.exports = class AuthController {
     res.render("auth/login");
   }
 
+  static async showLoginPost(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      req.flash("message", "Usuário nāo encontrado !!");
+      res.render("auth/login");
+      return;
+    }
+
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida !!");
+      res.render("auth/login");
+      return;
+    }
+
+    req.session.userid = user.id;
+
+    req.flash("message", "Autenticaçāo realizada com sucesso!!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static showRegister(req, res) {
     res.render("auth/register");
   }
--- projeto-toughts-nodejs/routes/authRoutes.js
+++ projeto-toughts-nodejs/routes/authRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 router.get('/login', AuthController.showLogin);
+router.post('/login', AuthController.showLoginPost);
 router.get('/register', AuthController.showRegister);
 router.post('/register', AuthController.showRegisterPost);
 router.get('/logout', AuthController.sessionLogout);
